864a4f6930d9e8f5fcee3543188597d4794a2576
Bharath Reddy <bharathmh@gmail.com>
2017-04-20 00:19:03 -0700

    first commit

---
+++ HamburgerView/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  HamburgerView
+//
+//  Created by Bharath D N on 4/19/17.
+//  Copyright © 2017 Bharath D N. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
